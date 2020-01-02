efa213ac0e5a6b5995dadebd5e17bd841458a586
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-01-02 02:04:24 -0400

    starter files

---
+++ kraig-test-owl/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+import participants from "./Participants";
+
+
+function App(props) {
+  return (
+    <div className="App">
+     <Participants id={} avatar={} inSession={}  onStage={}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ kraig-test-owl/src/Participants.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function Participants(props){
+
+    return (
+<div>These are the particpants</div>
+
+
+    )
+
+
+}
