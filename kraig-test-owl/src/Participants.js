cc575b7738577e5b90040dc4867372f2cb3fc7a2
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-01-02 04:17:58 -0400

    start styling stage component

--- kraig-test-owl/src/Participants.js
+++ kraig-test-owl/src/Participants.js
@@ -1,14 +1,15 @@
 import React from 'react';
-
+import './Participants.css';
 
 export default function Participants(props){
 
     return (
 <div>
-    <ul>
+    <ul class="list">
+    <img class="avatar" src={props.avatar} alt="avatar" />
     <li><h3>{props.name}</h3>
-    <img src={props.avatar} alt="avatar" />
-     {props.inSession===true ? <span>In Session</span>: <span>Logged Out</span>}     
+    
+     {props.onStage===true ? <p><span>on stage  </span><span class="boot">Boot</span></p>: <span></span>}     
     </li>
     </ul>
 </div>
